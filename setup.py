a4c24732e14cac15fff282ce1cda4b9f88b4f0df
Souvik Basak <souvikbasak2003@gmail.com>
2023-05-16 01:15:43 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+hyphen='-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+       with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+       if hyphen in requirements:
+        requirements.remove(hyphen)
+       return requirements
+
+setup(
+    name='mlfullproject',
+    version='0.0.1',
+    author='Souvik Basak',
+    author_email='souvikbasak2003@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
